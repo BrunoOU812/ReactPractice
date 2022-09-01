aa2b24b399e669c229cde43ef4d21d7e5476426b
Bruno Morgades <brunomorgades@hotmail.com>
2022-09-01 13:06:49 -0300

    eliminando App.jsx

--- App.jsx
+++
@@ -1,9 +0,0 @@
-class App extends React.Component{
-    render(){
-        return(<div>
-            <h1>Counter Game</h1>
-        </div>);
-    }
-}
-
-ReactDOM.render(<App/>,document.querySelector("#app"));
